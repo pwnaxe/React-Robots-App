20cc7fe732a92cc879a5dc9bb1083676636b61fb
pwnaxe <biuro.norsk@gmail.com>
2023-03-30 21:21:35 +0200

    +redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./containers/App";
+import reportWebVitals from "./reportWebVitals";
+import { searchRobots } from "./reducers";
+import "tachyons";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(searchRobots);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
